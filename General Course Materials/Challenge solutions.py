1cd561c757917e0aae8b41a61c20e4e883eed65d
nickleea23 <nickanderson08@yahoo.com>
2024-02-03 10:27:17 -0500

    Adding week 3 challenge solutions

--- General Course Materials/Challenge solutions.py
+++ General Course Materials/Challenge solutions.py
@@ -22,3 +22,39 @@ items + ["lobster"]
 
 
 
+###  Week 3 ===================================
+
+import numpy as np
+np.array(range(2,21,2))
+np.arange(2,21,2)
+
+
+## Gender challenge
+# solution 1
+gender = np.array(["Female", "F", "Female", "M", "Male"])  
+gender
+gender.astype('<U1') # Select first character, by converting to a type which only stores one byte
+
+# solution 2
+np.where(len(gender)==1, gender, gender.astype('<U1'))
+
+# solution 3
+np.where(np.where(gender == "Female", "F", gender) == "Male", "M", "NA")
+
+# solutoin 4
+np.where(np.in1d(gender, ['F', 'M']), gender, gender.astype('<U1'))
+
+
+## Linear algebra challenge
+# Generate data for this exercise
+rng = np.random.default_rng(seed=3252)
+x1 = rng.standard_normal(1000)
+x2 = rng.standard_normal(1000)
+Y = -1 + 2*x1 + 7*x2 + rng.standard_normal(1000)
+
+# Step 1
+X = np.column_stack((np.ones(1000), x1, x2))
+X
+#Step 2
+beta_hat = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+beta_hat
